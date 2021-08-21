e6b1f922a66466e1fced85be14eae5ea69188ce0
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-20 20:18:55 -0400

    Recursion Fix

--- recursive.py
+++ recursive.py
@@ -5,6 +5,12 @@ def recursive_knapsack(weight_cap, weights, values, i):
         return recursive_knapsack(weight_cap, weights, values, i - 1)
     else:
         include_item = values[i - 1] + recursive_knapsack(weight_cap - weights[i - 1], weights, values, i - 1)
-        exclude_item = recursive_knapsack(weight, weights, values, i - 1)
+        exclude_item = recursive_knapsack(weight_cap, weights, values, i - 1)
 
-        return max(include_item, exlucde_item)
+        return max(include_item, exclude_item)
+
+
+weight_cap = 50
+weights = [31, 10, 20, 19, 4, 3, 6]
+values = [70, 20, 39, 37, 7, 5, 10]
+print(recursive_knapsack(weight_cap, weights, values, len(values)))
